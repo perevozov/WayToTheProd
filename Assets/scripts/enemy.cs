3f60f1f53350d8a3fd9dd55d8c7b2b97b321de8d
Aleksei Perevozov <perevozov@*.perevozov.bx>
2019-06-15 10:53:36 +0200

    Enemy movement

---
+++ Assets/scripts/enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    public Rigidbody2D Rigidbody2D;
+    public float Speed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+        Rigidbody2D.velocity = Vector2.down * Speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
