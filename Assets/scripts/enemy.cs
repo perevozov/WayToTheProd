f60a7e090ecb213ea3b7f7feba5095046fe54d38
Aleksei Perevozov <perevozov@*.perevozov.bx>
2019-06-15 12:27:51 +0200

    Actual destroy

--- Assets/scripts/enemy.cs
+++ Assets/scripts/enemy.cs
@@ -11,17 +11,36 @@ public class enemy : MonoBehaviour
     void Start()
     {
         Rigidbody2D = GetComponent<Rigidbody2D>();
-        Rigidbody2D.velocity = Vector2.down * Speed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void FixedUpdate()
+    {
+        Rigidbody2D.velocity = Vector2.down * Speed;
     }
 
     public void StartFading()
     {
         Debug.Log("i'm gonna die!!");
+        Speed = 0;
+
+        StartCoroutine("Fade");
+    }
+
+    IEnumerator Fade()
+    {
+        for (float f = 1f; f >= 0; f -= 0.1f)
+        {
+            Color c = GetComponent<Renderer>().material.color;
+            c.a = f;
+            GetComponent<Renderer>().material.color = c;
+            yield return new WaitForSeconds(.1f);
+        }
+        Destroy(this.gameObject);
     }
 }
